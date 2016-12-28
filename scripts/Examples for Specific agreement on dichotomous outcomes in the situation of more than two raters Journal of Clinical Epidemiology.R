20c646bb46597adceeefd2f514b7f39333503212
Iris Eekhout <iriseekhout@gmail.com>
2016-12-28 12:20:12 +0100

    example paper 1

--- scripts/Examples for Specific agreement on dichotomous outcomes in the situation of more than two raters Journal of Clinical Epidemiology.R
+++ scripts/Examples for Specific agreement on dichotomous outcomes in the situation of more than two raters Journal of Clinical Epidemiology.R
@@ -6,21 +6,18 @@ data(breast)
 variables_available <- c("volume", "shape", "symmetry", "scars", "nipple")
 raters_available <- c("Patient", "PCH1", "PCH2", "PCH3", "PCH4", "PCH5", "Mam1", "Mam2", "Mam3")
 
-#!# dichotomiseren ratersdata
+#!# dichotomiseren ratersdata 1 means satisfied, 2 means not satisfied
 datdich <- breast
 for (v in 1:length(variables_available)){
   ratersvars1 <- paste(raters_available, variables_available[v], sep="_")
   for (r in 1:length(ratersvars1)){
-    datdich[ratersvars1[r]] <- ifelse(datdich[ratersvars1[r]]>3,2,1)
+    datdich[ratersvars1[r]] <- ifelse(datdich[ratersvars1[r]]>3,1,2)
   }
 }
 
 
-#!# hier aangeven welke variabele je wilt bekijken van de bovenstaande variables available.
 variable <- variables_available[3]
-#!# hier selecteren welke raters je wilt vergelijken van de bovenstaande raters available.
 raters <- c(raters_available[2:5])
-#!# aantal categorien Likert schaal
 ratersvars <- paste(raters, variable, sep="_")
 
 data1 <- data.frame(apply(datdich[ratersvars],2,as.factor))
@@ -34,4 +31,16 @@ specific.agreement1(sumtab,"positive")
 specific.agreement1(sumtab,"negative")
 
 CIagreement(agreement(sumtab),n=nrow(data1),m=4)
+
+
+#######################
+#appendix A.6 example
+#Calculation of observed agreement and specific agreements for the situation of 86% “satisfied’ scores
+
+table1<-matrix(c(220,38,38,4),nrow=2,byrow = TRUE)
+
+agreement(table1)
+
+specific.agreement1(table1,"positive")
+specific.agreement1(table1,"negative")
 
