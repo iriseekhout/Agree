f2c0e3bffcd06eb19ee06178ef9758f2b95534e6
Iris Eekhout <iriseekhout@gmail.com>
2016-12-28 12:20:23 +0100

    Examples with paper 2

---
+++ scripts/Examples for Specific agreement on polytomous outcomes in the situation of more than two raters Journal of Clinical Epidemiology.R
@@ -0,0 +1,13 @@
+#Examples paper 2 (polytomous data, multiple raters)
+# Title: Specific agreement on polytomous outcomes in the situation of more than two raters Journal of Clinical Epidemiology
+
+
+## ordinal data example
+
+breast
+
+
+
+## nominal data example
+
+diagnoses
