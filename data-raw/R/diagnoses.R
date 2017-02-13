8461139efce5e8503f077041de06d6f653ca8837
iriseekhout <iriseekhout@gmail.com>
2017-02-13 16:22:41 +0100

    Change rater order in diagnoses data

--- data-raw/R/diagnoses.R
+++ data-raw/R/diagnoses.R
@@ -3,6 +3,9 @@
 library(irr)
 data(diagnoses)
 
+diagnoses <- data.frame(diagnoses$rater5, diagnoses$rater1, diagnoses$rater3, diagnoses$rater2, diagnoses$rater6, diagnoses$rater4)
+colnames(diagnoses) <- c("rater5","rater1" ,"rater3", "rater2", "rater6", "rater4")
+
 
 save("diagnoses", file = file.path("C:/Users/eekhouti/Github/Agree/data/diagnoses.rda"),
      compress = "xz")
