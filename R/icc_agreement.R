6e7ef676e914480481801c34b8a8a65367c6117f
iriseekhout <iriseekhout@gmail.com>
2020-06-19 15:39:23 +0200

    checks on ci calculations for icc_agreement.

--- R/icc_agreement.R
+++ R/icc_agreement.R
@@ -1,4 +1,3 @@
-
 #' ICC agreement
 #'
 #' @param model merMod object result from \code{icc_model()}.
@@ -23,6 +22,8 @@ icc_agreement <- function(model, alpha = 0.05){
   MSBt <- 1 +(k - 1)*icc_a #test
   MSEt <- 1-icc_a #test
   F_a1 <- (n * varobs_agr + varerr_agr)/varerr_agr
+  JMS <- #changed after Shrout and Fleis check
+  EMS <- #changed after Shrout and Fleis check
 
   Fa1 <- MSBt/MSEt #test
   Fl <- qf(alpha/2, df1= (n-1) ,df2=n*(k-1))
@@ -33,17 +34,20 @@ icc_agreement <- function(model, alpha = 0.05){
   #where Fl and Fu are the ˛=2 and the 1  ˛=2th quantiles of the F -distribution    with degrees of freedom
   # N  1 and N.k  1/, respectively.
 
-  vn <- (k - 1) * (n - 1) * ((k * icc_a * F_a1 + n *
-                                (1 + (k - 1) * icc_a) - k * icc_a))^2
-  vd <- (n - 1) * k^2 * icc_a^2 * F_a1^2 + (n * (1 + (k - 1) * icc_a) - k * icc_a)^2
-  v <- vn/vd
-  F3U <- qf(1 - alpha/2, n - 1, v)#or alpha/2?not dividing by 2 is shrout fleis
-  F3L <- qf(1 - alpha/2, v, n - 1)#or alpha/2?
-  L_a <- n * (MSB - F3U * varerr_agr)/(F3U * (k * (n * varobs_agr + varerr_agr) + (k *
-                                                                                     n - k - n) * varerr_agr) + n * MSB)#
 
-  U_a <- n * (F3L * MSB - varerr_agr)/(k * (n * varobs_agr + varerr_agr) + (k * n -
-                                                                              k - n) * varerr_agr + n * F3L * MSB)
+  ## following Shrout & Fleis 1979 - Fleiss & Shrout 1978
+  F_a1 <- (n * varobs_agr + varerr_agr)/(varerr_agr) #Shrout and Fleis check === not sure on the first part (RMS)
+
+  vn <- (k - 1) * (n - 1) * (k * icc_a * F_a1 + n * (1 + (k - 1) * icc_a) - k * icc_a)^2 #checked with shrout fleis.
+  vd <- (n - 1) * k^2 * icc_a^2 * F_a1^2 + (n * (1 + (k - 1) * icc_a) - k * icc_a)^2 #checked with shrout fleis.
+  v <- vn/vd #checked with shrout fleis.
+  F3U <- qf(1 - alpha/2, n - 1, v)#Shrout and fleis checked
+  F3L <- qf(1 - alpha/2, v, n - 1)##checked with shrout fleis.
+  L_a <- (n * (MSB - F3U * varerr_agr))/
+    (F3U * (k * (n * varobs_agr + varerr_agr) + (k * n - k - n) * varerr_agr) + n * MSB)##checked with shrout fleis.
+
+  U_a <- (n * (F3L * MSB - varerr_agr))/(k * (n * varobs_agr + varerr_agr) + (k * n -
+                                                                              k - n) * varerr_agr + n * F3L * MSB)#checked with shrout fleis.
 
   return(
     list(
@@ -52,8 +56,8 @@ icc_agreement <- function(model, alpha = 0.05){
       varerr_agr = varerr_agr,
       icc_a = icc_a,
       sem_a = sem_a,
-      L_a = l_a, #use upper case L for PSYCH version
-      U_a = u_a #use upper case U for PSYCH version
+      L_a = l_a, #use upper case L for PSYCH version/ lowercase l for same logic as in oneway and consistency - however the observer part is then ignored.
+      U_a = u_a #use upper case U for PSYCH version/ lowercase l for same logic as in oneway and consistency - however the observer part is then ignored.
     )
   )
 }
