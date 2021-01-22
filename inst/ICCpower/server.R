72a7dc9bef1f361511ac4cab98591574d6c1cb84
Iris Eekhout <iriseekhout@gmail.com>
2021-01-22 14:18:16 +0100

    setup with simulation

--- inst/ICCpower/server.R
+++ inst/ICCpower/server.R
@@ -8,19 +8,65 @@
 #
 
 library(shiny)
+library(MASS)
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+    # simulation page ----
+    nseq <- reactive({
+        seq(from = input$n[1], to = input$n[2], length.out = input$nlength)
     })
+    nseq <- seq(from = 10, to = 100, length.out = 5)
+
+    kseq <- reactive({
+        seq(from = input$k[1], to = input$k[2], by =1)
+    })
+    kseq <- seq(from = 2, to = 5, by = 1)
+
+    mean = 0
+    correlation = 0.7
+    variance = 1
+
+    output <- vector()
+    for(ni in nseq){ #nseq = nseq()
+        for(ki in kseq){ #kseq = kseq()
+            for(i in 1:100){
+                means <- rep(mean, ki) #mean = input$mean kr = kr()
+                cov <- matrix(correlation,ki, ki) #correlation = input$correlation
+                    # set up variance
+                diag(cov) <- 1
+                cov <- cov* variance #variance = input$variance
+
+                dat <- as.data.frame(mvrnorm(means, cov, n=ni))
+
+                measures <- Agree::icc(data = dat, var = TRUE)
+
+
+                iteration <- c(measures["oneway",], measures["agreement",], measures["consistency",])
+
+                iteration <- c(measures["oneway",], measures["agreement",], measures["consistency",])
+
+
+                names(iteration) <- c(paste(names(iteration)[1:7], "oneway", sep = "_"),
+                                      paste(names(iteration)[8:14], "agr", sep = "_"),
+                                      paste(names(iteration)[15:21], "cons", sep = "_"))
+                iteration <- c(set=i,n=ni, k=ki, iteration)
+
+
+                output <- rbind(output, iteration) #stack each simulation iteration in rows (end of simulation run, 1000 rows)
+
+
+            }
+        }
+    }
+    # settings page ----
+
+    # background page ----
 
 })
+
+
+## to do:
+## use toggle hide show to show relevant pages at relevant times. - works now for D-score "results". adjust for question part to
+
--- inst/ICCpower/ui.R
+++ inst/ICCpower/ui.R
@@ -7,27 +7,148 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
+
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
+ui <- navbarPage(
+    theme = shinytheme("cerulean"), collapsible = TRUE,
+    "ICC power",
+    #simulation page ----
+    tabPanel("Simulate",
+             sidebarLayout(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+                 sidebarPanel( #shinyjs::useShinyjs(),
+                               tags$h2("Study Design"),
+                               tags$p("Select study design options"),
+                               radioButtons("icc",
+                                         label = "Type of ICC",
+                                         choices = c("ICConeway", "ICCagreement", "ICCconsistency")),
+                               numericInput("correlation",
+                                            label = "Expected correlation between raters",
+                                            value = 0.7,
+                                            min = 0, max = 1),
+                               numericInput("variance",
+                                            label = "Expected variance in scores",
+                                            value = 1),
+                               numericInput("mean",
+                                            label = "Expected mean of score",
+                                            value = 1,
+                                            ),
+                               sliderInput("k",
+                                           label = "number of raters",
+                                           min = 1, max = 20,
+                                           value = c(2, 10),
+                                           step = 1),
+                               sliderInput("n",
+                                           label = "sample size",
+                                           min = 0, max = 1000,
+                                           value = c(10, 200)),
+                               numericInput("nlength",
+                                            "steps in sample size sequence")
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+                              div(id = "main_start0",
+                                   textOutput("agemos")
+                               )%>% shinyjs::hidden(),
+                               #tags$br(),
+                               #actionButton("start", "Start"),
+                               #tags$br(),
+                               #tags$br()
+                               width = 3),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
-))
+                 mainPanel(
+
+             )
+    ),
+    #result page ----
+    # tabPanel("D-score",
+    #          div(id = "results",
+    #              sidebarLayout(
+    #                  sidebarPanel(
+    #                      tableOutput("checkanswers")
+    #
+    #                  ),
+    #                  mainPanel(
+    #                      fluidRow(
+    #                          plotOutput("scoreplot"),
+    #                          box(width = 6,
+    #                              tableOutput("scoretab"),
+    #                          )),
+    #                      fluidRow(
+    #                          downloadButton(
+    #                              outputId = "downloadReport",
+    #                              label = "Download Report"
+    #                          ))
+    #                  )
+    #                  #add download button to download results
+    #              )
+    #          ) %>% shinyjs::hidden()
+    # ),
+    # #settings page ----
+    # tabPanel("Settings",
+    #          ##add choice of itembank
+    #          fluidRow(
+    #              box(with = 12,
+    #                  tags$h3("Itembank selection"),
+    #                  tags$p("Select one of the available itembanks in the box below. The GSED LF is the GSED long form database, with directly observed items. The GSED SF is the GSED short form, with caregiver reported items. The GCGD 165 is an itembank with 165, mostly directly observed, items selected by the Global Child Development Group. Van Wiechen is the Dutch Developmental Inventory (Dutch questions)"),
+    #                  selectInput("itembank",
+    #                              label = "Select itembank",
+    #                              choices = c("GSED LF",
+    #                                          "GSED SF",
+    #                                          "GCDG 165",
+    #                                          "Van Wiechen",
+    #                                          "GSED LF stream A",
+    #                                          "GSED LF stream B",
+    #                                          "GSED LF stream C"),
+    #                              selected = "GSED LF"),
+    #                  ##add choice of stop rule
+    #                  tags$h3("Stop rule"),
+    #                  tags$p("Below the settings for the stop rule can be defined. By default the test stops, when the Standard error of measurement (SEM) of the D-score is below 1. A higher SEM threshold leads to a shorter test, but with a higher measurement error around the D-score. A lower SEM threshold means a smaller error around the D-score but also a longer test (i.e. more questions asked."),
+    #                  numericInput("semrule",
+    #                               label = "Threshold for SEM as stopping rule",
+    #                               value = 1,
+    #                               min = 0, max = 10,
+    #                               step = 0.1
+    #                  ),
+    #                  ##add choice of item leniency
+    #                  tags$h3("Item leniency"),
+    #                  tags$p("The leniency for the items is the probability for passing the next question, given the previous answers. The default leniency is set at 0.5. This means that the probability for passing the next item is 0.5. When the leniency is set to a higher probability, the items are easier to pass (given previous answers), but it means that there are more items needed to finish the test."),
+    #                  sliderInput("leniency",
+    #                              label = "Leniency for selecting the next item",
+    #                              min = 0,
+    #                              max = 1,
+    #                              value = 0.5,
+    #                              step = 0.05)
+    #              )
+    #          )
+    # ),
+    # #info page ----
+    # navbarMenu("Background",
+    #            #checklist info ----
+    #            tabPanel("Itembanks"),
+    #            #calculation info ----
+    #            tabPanel("D-score"),
+    #            #interpretation info ----
+    #            tabPanel("Interpretation")),
+    # tabPanel("About",
+    #          fluidRow(
+    #              box(
+    #                  tags$h2("Demo application for D-score adaptive test"),
+    #                  tags$p("This is a demo version developed to demonstrate how an adaptive test works for measuring D-score. This version is developed for demonstration and illustrative purposes and should not be used in research or practice in its current form")
+    #              ),
+    #              box(width = 12, title = "About the authors",
+    #                  tags$a(img(src="TNO_zwart.jpg", width="10%"),href="https://www.tno.nl/nl/"),tags$p("Department Child Health"),
+    #                  tags$br(),
+    #                  tags$h4("Iris Eekhout"),
+    #                  tags$div("Email: iris.eekhout@tno.nl"),
+    #                  tags$div("Website: ",tags$a("www.iriseekhout.com", href="https://www.iriseekhout.com")),
+    #                  tags$div("Github: ", tags$a("iriseekhout", href="https://github.com/iriseekhout"))
+    #
+    #              ),
+    #              box(width= 12,
+    #                  tags$h4("GSED consortium"),
+    #                  tags$div("The data and instrument content and information was developed by the GSED consortium.")
+    #              )
+    #          )
+ )
+
+)
